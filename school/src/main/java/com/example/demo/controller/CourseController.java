6a06b2dc8cd79b10df174dec348a766006c33afe
tedidimitrov <teodor_dimitrov98@abv.bg>
2023-08-29 21:01:49 +0300

    Add project

--- school/src/main/java/com/example/demo/controller/CourseController.java
+++ school/src/main/java/com/example/demo/controller/CourseController.java
@@ -1,2 +1,39 @@
-package com.example.demo.controller;public class CourseController {
+package com.example.demo.controller;
+
+import com.example.demo.model.Course;
+import com.example.demo.model.CourseType;
+import com.example.demo.service.CourseService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/course")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @Autowired
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE},
+                 produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Course> createCourse(@RequestBody @Valid Course course) {
+        return new ResponseEntity<>(courseService.createCourse(course), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/total")
+    public ResponseEntity<String> getCoursesCount() {
+        return new ResponseEntity<>(courseService.getCoursesCount(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "{type}")
+    public ResponseEntity<String> getCoursesCountByType(@PathVariable("type") CourseType type) {
+        return new ResponseEntity<>(courseService.getCoursesCountByType(type), HttpStatus.OK);
+    }
 }
